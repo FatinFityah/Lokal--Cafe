bdf3c402a42d04c3dedb864136ff6533411e8ca5
FatinFityah <126367282+FatinFityah@users.noreply.github.com>
2025-11-28 14:32:10 +0800

    Update db.js

--- js/db.js
+++ js/db.js
@@ -4,23 +4,22 @@
 
 const products = [
   // FOOD
-  { id: "f1", name: "Nasi Ayam Crunchy", price: 12.00, img: "images/nasi_ayam.jpg" },
-  { id: "f2", name: "Nasi Lemak Kukus Biasa", price: 5.00, img: "images/nasi_lemak.jpg" },
-  { id: "f3", name: "Nasi Lemak Buttermilk", price: 15.00, img: "images/nl_buttermilk.jpg" },
-  { id: "f4", name: "Fettuccine Buttermilk", price: 14.00, img: "images/fettuccine.jpg" },
-  { id: "f5", name: "Spaghetti Bolognese", price: 13.00, img: "images/bolognese.jpg" },
-  { id: "f6", name: "Spaghetti Carbonara", price: 13.00, img: "images/carbonara.jpg" },
+  { id: "f1", name: "Nasi Ayam Crunchy", price: 12.00, img: "images/nasi_ayam_crunchy.jpg" },
+  { id: "f2", name: "Nasi Lemak Kukus Biasa", price: 5.00, img: "images/nasi_lemak_kukus_biasa.jpg" },
+  { id: "f3", name: "Nasi Lemak Buttermilk", price: 15.00, img: "images/nasilemak_kukus_buttermilk.jpg" },
+  { id: "f4", name: "Fettuccine Buttermilk", price: 14.00, img: "images/fettucine_buttermilk.jpg" },
+  { id: "f5", name: "Spaghetti Bolognese", price: 13.00, img: "images/spaghetti_bolognese.jpg" },
+  { id: "f6", name: "Spaghetti Carbonara", price: 13.00, img: "images/spaghetti_carbonara.jpg" },
   { id: "f7", name: "Chicken Chop", price: 16.00, img: "images/chicken_chop.jpg" },
   
   // BEVERAGES
   { id: "b1", name: "Sweet Strawberry Frappe", price: 8.00, img: "images/straw_frappe.jpg" },
   { id: "b2", name: "Blue Monster Frappe", price: 9.00, img: "images/blue_monster.jpg" },
   { id: "b3", name: "Vietnam Frappe", price: 9.00, img: "images/vietnam_frappe.jpg" },
-  { id: "b4", name: "Vanilla Cookies Frappe", price: 9.50, img: "images/vanilla_frappe.jpg" },
-  { id: "b5", name: "Butterscotch Frappe", price: 9.50, img: "images/butterscotch.jpg" },
-  { id: "b6", name: "Banana Frappe", price: 8.50, img: "images/banana_frappe.jpg" },
-  { id: "b7", name: "Chocolate Larva Frappe", price: 10.00, img: "images/choc_larva.jpg" },
-  { id: "b8", name: "Mineral Water", price: 2.00, img: "images/water.jpg" },
+  { id: "b4", name: "Butterscotch Frappe", price: 9.50, img: "images/butterscotch.jpg" },
+  { id: "b5", name: "Banana Frappe", price: 8.50, img: "images/banana_frappe.jpg" },
+  { id: "b6", name: "Chocolate Larva Frappe", price: 10.00, img: "images/choc_larva.jpg" },
+  { id: "b7", name: "Mineral Water", price: 2.00, img: "images/water.jpg" },
 
   // DESSERT
   { id: "d1", name: "Strawberry Waffle", price: 6.00, img: "images/straw_waffle.jpg" },
